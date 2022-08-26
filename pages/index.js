e3262d8d2b6a071a9d2cd72f6821a8a38ee673e4
nittashiori <nitta@impv.co.jp>
2022-08-26 11:00:17 +0900

    カスタムAppコンポーネントにLayoutを全ページに入れる

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-import Layout from "components/layout"
+
 import Hero from "components/hero"
 
 export default function Home() {
   return (
-    <Layout>
-      <Hero />
-    </Layout>
+    <Hero />
   )
 }
