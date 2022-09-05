b8a1cdb39178879ae8c03820c2b01f6ab3ea272d
nittashiori <nitta@impv.co.jp>
2022-09-05 11:22:04 +0900

    サイトに関する情報を共有できる形で用意する

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,3 +1,4 @@
+import Meta from "components/meta"
 import Container from "components/container"
 import Hero from "components/hero"
 
@@ -5,6 +6,7 @@ export default function Blog() {
   return (
     <>
       <Container>
+        <Meta pageTitle="ブログ" />
         <Hero
           title="Blog"
           subtitle="Recent Posts"
