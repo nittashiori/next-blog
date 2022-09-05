5c677f00c3e675dbc706051f3374b30334e4415c
nittashiori <nitta@impv.co.jp>
2022-09-05 12:03:55 +0900

    サイトに関する情報とアイコン画像を追加する

--- components/meta.js
+++ components/meta.js
@@ -20,8 +20,13 @@ export default function Meta({ pageTitle, pageDesc }) {
       <meta property="og:title" content={title} />
       <meta property="description" content={desc} />
       <meta property="og:description" content={desc} />
-      <link rel="stylesheet" href={url} />
+      <link rel="canonical" href={url} />
       <meta property="og:url" content={url} />
+      <meta property="og:site_name" content={siteTitle} />
+      <meta property="og:type" content={siteType} />
+      <meta property="og:locale" content={siteLocale} />
+      <link rel="icon" href={siteIcon} />
+      <link rel="apple-touch-icon" href={siteIcon} />
     </Head>
   )
 }
