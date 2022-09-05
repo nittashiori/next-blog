eb76467f5ac111aaa10a132a94061779b3893c32
nittashiori <nitta@impv.co.jp>
2022-09-05 12:13:33 +0900

    ページタイトル以外のメタデータを追加する

--- components/meta.js
+++ components/meta.js
@@ -5,7 +5,10 @@ import { useRouter } from "next/router"
 import { siteMeta } from "lib/constants"
 const { siteTitle, siteDesc, siteUrl, siteLocale, siteType, siteIcon } = siteMeta
 
-export default function Meta({ pageTitle, pageDesc }) {
+// 汎用OGP画像
+import siteImg from "images/ogp.jpg"
+
+export default function Meta({ pageTitle, pageDesc, pageImg, pageImgW, pageImgH }) {
   // ページのタイトル（ページタイトルが未指定な場合の処理を指定する）
   const title = pageTitle ? `${pageTitle} | ${siteTitle}` : siteTitle
   // ページの説明
@@ -13,6 +16,13 @@ export default function Meta({ pageTitle, pageDesc }) {
   // ページのURL
   const router = useRouter()
   const url = `${siteUrl}${router.asPath}`
+  // OGP画像
+  // 画像をpageImgに、pageImgがない場合はsiteImgに設定
+  const img = pageImg || siteImg.src
+  const imgW = pageImgW || siteImg.width
+  const imgH = pageImgH || siteImg.height
+  // startsWith()メソッドを使って確認し、必要に応じてsiteUrlを付加。
+  const imgUrl = img.startsWith("https://") ? img : `${siteUrl}${img}`
 
   return (
     <Head>
@@ -27,6 +37,10 @@ export default function Meta({ pageTitle, pageDesc }) {
       <meta property="og:locale" content={siteLocale} />
       <link rel="icon" href={siteIcon} />
       <link rel="apple-touch-icon" href={siteIcon} />
+      <meta property="og:image" content={imgUrl} />
+      <meta property="og:image:width" content={imgW} />
+      <meta property="og:image:height" content={imgH} />
+      <meta name="twitter:card" content="summary_large_image" />
     </Head>
   )
 }
