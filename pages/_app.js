ef5ac00378eebfe9cd999bb40d96cfaf7d616661
nittashiori <nitta@impv.co.jp>
2022-09-21 16:04:46 +0900

    useEffectを使ってGoogleアナリティクスを設定する

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,49 @@
+import { useEffect } from "react"
+import { useRouter } from "next/router"
 import "styles/globals.css"
 import Layout from "components/layout"
+import Script from 'next/script'
+import * as gtag from "lib/gtag"
 // Font Awesomeの設定
 import "@fortawesome/fontawesome-svg-core/styles.css"
 import { config } from "@fortawesome/fontawesome-svg-core"
 config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on("routeChangeComplate", handleRouteChange) //イベントリスナーの登録
+    return () => {
+      router.events.off("routeChangeComplate", handleRouteChange) //イベントリスナーの削除
+    }
+  }, [router.events])
+
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          _html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${gtag.GA_MEASUREMENT_ID}');
+          `
+        }}
+      />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   )
 }
 
