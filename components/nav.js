942001244b0913fa1fd5e1f67d0a839f8c90aa9b
nittashiori <nitta@impv.co.jp>
2022-09-21 13:39:27 +0900

    リンクをクリックしたらメニューを閉じる

--- components/nav.js
+++ components/nav.js
@@ -9,23 +9,27 @@ export default function Nav() {
     setNavIsOpen((prev) => !prev)
   }
 
+  const closeNav = () => {
+    setNavIsOpen(false)
+  }
+
   return (
     <nav className={navIsOpen ? styles.open : styles.close}>
       <button className={styles.btn} onClick={toggleNav}>MENU</button>
       <ul className={styles.list}>
         <li>
           <Link href="/">
-            <a>Home</a>
+            <a onClick={closeNav}>Home</a>
           </Link>
         </li>
         <li>
           <Link href="/about">
-            <a>About</a>
+            <a onClick={closeNav}>About</a>
           </Link>
         </li>
         <li>
           <Link href="/blog">
-            <a>Blog</a>
+            <a onClick={closeNav}>Blog</a>
           </Link>
         </li>
       </ul>
