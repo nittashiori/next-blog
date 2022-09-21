2bd54899d9cd683a5bb66f076b6059ec89451bbf
nittashiori <nitta@impv.co.jp>
2022-09-21 13:37:56 +0900

    stateに応じてメニューを開閉する

--- components/nav.js
+++ components/nav.js
@@ -1,9 +1,17 @@
+import { useState } from "react"
 import Link from "next/link"
 import styles from "styles/nav.module.css"
 
 export default function Nav() {
+  const [navIsOpen, setNavIsOpen] = useState(false)
+
+  const toggleNav = () => {
+    setNavIsOpen((prev) => !prev)
+  }
+
   return (
-    <nav>
+    <nav className={navIsOpen ? styles.open : styles.close}>
+      <button className={styles.btn} onClick={toggleNav}>MENU</button>
       <ul className={styles.list}>
         <li>
           <Link href="/">
